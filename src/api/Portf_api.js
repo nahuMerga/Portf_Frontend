e018e24ffe31335fd54527e0ddfcbd451c53d5c2
NahoMer <143824687+nahuMerga@users.noreply.github.com>
2025-02-24 16:05:21 -0300

    Update Portf_api.js

--- src/api/Portf_api.js
+++ src/api/Portf_api.js
@@ -9,7 +9,6 @@ axios.defaults.withCredentials = true;
 const api = axios.create({
     baseURL: `${BASE_URL}/portf_api/`, 
     headers: {
-        'Authorization': `Bearer ${localStorage.getItem('access_token')}`,
         'Content-Type': 'application/json',
     },
     withCredentials: true 
